3b438bcfff750a0a28f3c2c7eb4866a797296750
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2020-12-04 05:21:39 +0100

    init

---
+++ toggle_any_setting.py
@@ -0,0 +1,24 @@
+import sublime
+import sublime_plugin
+
+class ToggleAnySettingCommand(sublime_plugin.WindowCommand):
+	def run(self, settings_base_name="Preferences.sublime-settings" , setting="dummy_setting"):
+		self.settings = sublime.load_settings(settings_base_name)
+		state = self.settings.get(setting)
+		
+		#print("previously:", self.settings.get(setting))
+
+		if state == True:
+			self.settings.set(setting,False)
+		elif state == False:
+			self.settings.set(setting,True)
+		else:
+			print("setting not present")
+
+		sublime.save_settings(settings_base_name)
+		settings_list = [self.settings]
+
+		#print("currently:", self.settings.get(setting))
+
+# Test in console:
+# window.run_command("toggle_any_setting", {"setting": "auto_complete"})
